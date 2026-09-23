55b07d714d3ae787b05cb84390ffbf49afdaaf4e
Mercado <kristle.g.s.mercado@accenture.com>
2026-09-23 22:43:03 +0800

    Remove limit enforcement on JS

--- blocks/xe-feature-cards/xe-feature-cards.js
+++ blocks/xe-feature-cards/xe-feature-cards.js
@@ -17,24 +17,13 @@ await Promise.all([
   '../../scripts/ignite/bundle/compositions/feature-cards/xe-feature-cards.js',
 ].map(safeImport));
 
-const MIN_CARDS = 2;
-const MAX_CARDS = 3;
-
 export default function decorate(block) {
   const [heading, subHeading, ...allCards] = [...block.children];
-  if (allCards.length > MAX_CARDS) {
-    // eslint-disable-next-line no-console
-    console.warn(`xe-feature-cards expects at most ${MAX_CARDS} xe-card items, but found ${allCards.length}. Extra cards are hidden.`);
-  }
-  const cards = allCards.slice(0, MAX_CARDS);
-  while (cards.length < MIN_CARDS) {
-    cards.push(createCardBlock());
-  }
   const xeCardGrid = document.createElement('xe-card-grid');
   const featureCards = document.createElement('xe-feature-cards');
   featureCards.setAttribute('heading', heading.textContent);
   featureCards.setAttribute('subHeading', subHeading.textContent);
-  cards.forEach((row) => {
+  allCards.forEach((row) => {
     decorateCard(row);
     xeCardGrid.append(row);
   });
